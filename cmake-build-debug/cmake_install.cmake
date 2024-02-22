15ad951385db6dbcd57dbc6840502d9d891ff6ec
ast12233344444 <ahmet.turkel@kivitechnologies.com>
2024-02-22 20:38:56 +0300

    sgbs
    fva gsgbgs

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ast12/CLionProjects/extpr
+# Install script for directory: C:/Users/ast12/CLionProjects/extpr/.idea/cpp-arb
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ast12/CLionProjects/extpr/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/ast12/CLionProjects/extpr/.idea/cpp-arb/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
